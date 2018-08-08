29f668d2d8f89b5f948488025d72731807c58626
MitchellStride <Mitchellcstride@gmail.com>
2018-08-08 15:01:41 -0230

    working version of test code, lantronix crystal issues, i2c switch and eeprom left

--- Nucleo - STM32/GPMB Test Code/src/main.cpp
+++ Nucleo - STM32/GPMB Test Code/src/main.cpp
@@ -1,7 +1,7 @@
 #include <mbed.h>
 #include <BME280.h>
 #include <DS1820.h>
-
+#include <MCP23008.hpp>
 
 //Pin Assignments
 DigitalOut userLED1(PC_9);
@@ -22,16 +22,21 @@ DigitalIn sync(PC_13);
 
 AnalogOut DAC1_OUT(PA_4);
 AnalogIn P9_Ain1(PC_0);
-AnalogIn P9_Ain1(PC_0);
-AnalogIn P9_Ain1(PC_0);
+AnalogIn P9_Ain2(PC_1);
+AnalogIn P9_Ain3(PC_2);
 //AnalogIn LM35(PA_7);
 
 I2C i2c(PB_7, PB_6);
+CAN can1(PA_11, PA_12);
+
 
  //Init Serial for debug
 Serial xport(PA_2, PA_3); //Lantronix UART
 
+
+
 //Devices
+//MCP23008 mcp(PB_7, PB_6, 4);
 BME280 BME(PB_7, PB_6);
 DS1820  ds1820(PC_3);
 
@@ -39,11 +44,14 @@ DS1820  ds1820(PC_3);
 float pressure;
 float tempBME;
 float humidity;
+float AnaValue;
+char counter =0;
 
 //float tempLM35;
 
 int l1;
 int l2;
+int switchinputs = 0x00;
 
 int main() {
     //Setup Code
@@ -53,14 +61,16 @@ int main() {
     i2c.frequency(100000);
 
     //I2C I/O EXPANDER
-            //SWITCH CODE
+    //mcp.set_input_pins(0xFF);
 
     while(1) {
         //Loop Code
 
+      //  switchinputs = mcp.read_inputs();
+        xport.printf("\n\rRotary Inputs:%i", switchinputs);
         //User LEDs
-        /*userLED1 = !userLED1;
-        userLED2 = userLED1; */
+        userLED1 = !userLED1;
+        userLED2 = userLED1; 
         
         //BME280
         xport.printf("\n\r\n\r\n\rEnviromental Monitoring:\n\r");
@@ -89,50 +99,62 @@ int main() {
         xport.printf("leak1:%i\n\r", l1);
         xport.printf("leak2:%i\n\r", l2);
             //LEAK LEDS
-        if(l1)
+        /*if(l1)
             userLED1 = 1;
         else
             userLED1 = 0;
         if (l2)
             userLED2 = 1;
         else
-            userLED2 = 0;
+            userLED2 = 0;*/
 
         //Load Switches
-        LOAD0 = 1; //Turn on Load Switches
-        LOAD1 = 1;
+        LOAD0 = 0; //Turn on Load Switches
+        LOAD1 = 0;
 
         //SYNC Testing
         if(!sync){
-            userLED1 = 1;
+           // userLED1 = 1;
             xport.printf("\n\rSYNC pulled LOW\r\n");
-         } else
-            userLED1 = 0;
-
+         } else{
+           // userLED1 = 0;
+           }
         //Analog IN Header
-        float AnValue = P9_Ain1.read();
-        DAC1_OUT.write(1);
-        xport.printf("DAC-OUT:%.2fV\n\r", AnValue);
+        DAC1_OUT.write(0.5);
+        AnaValue = P9_Ain1.read();
+        wait_ms(50);
+        xport.printf("Ain1:%.2f V\n\r", AnaValue);
+        AnaValue = P9_Ain2.read();
+        wait_ms(50);
+        xport.printf("Ain2:%.2f V\n\r",  AnaValue);
+        AnaValue = P9_Ain3.read();
+        wait_ms(50);
+        xport.printf("Ain3:%.2f V\n\r",  AnaValue);
 
         //GPIO Isolater TEST
         SPI3_SCK = 1;
         SPI3_MOSI = 1;
-
-        
-        if(SPI3_MISO)
-            userLED1 = 1;
-          else
-            userLED1 = 0;
-        if(SPI3_SS)
-            userLED2 = 1;
-          else
-            userLED2 = 0;
-        
         BI_ISO_GPIO1 = 1;
         BI_ISO_GPIO2 = 1;
+         /*if(SPI3_MISO)
+            userLED1 = 1;
+        else
+            userLED1 = 0;
+         if(SPI3_SS)
+            userLED2 = 1;
+         else
+            userLED2 = 0;*/
+      
 
         //CAN
-
+        printf("\n\rCAN MSG SENDING");
+    if (can1.write(CANMessage(1337, &counter, 1))) {
+        counter++;
+        userLED1 = 1;
+        xport.printf("     SENT%i", counter);
+        wait(0.2);
+        userLED1 = 0;
+    }
         //RS232/485
 
         //IO
